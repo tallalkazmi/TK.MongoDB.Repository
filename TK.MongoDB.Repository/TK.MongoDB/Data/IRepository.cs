5ea2b94c541cd43d8069d906d24bee1a235d6110
Tallal Kazmi <tallalkazmi@gmail.com>
2020-10-13 11:04:22 +0400

    Sync methods added

--- TK.MongoDB.Repository/TK.MongoDB/Data/IRepository.cs
+++ TK.MongoDB.Repository/TK.MongoDB/Data/IRepository.cs
@@ -27,11 +27,25 @@ namespace TK.MongoDB.Data
         Task<T> FindAsync(Expression<Func<T, bool>> condition);
 
         /// <summary>
+        /// Find single document by condition specified.
+        /// </summary>
+        /// <param name="condition">Lamda expression</param>
+        /// <returns>Document</returns>
+        T Find(Expression<Func<T, bool>> condition);
+
+        /// <summary>
         /// Gets document by Id.
         /// </summary>
         /// <param name="id">Key</param>
         /// <returns>Document</returns>
         Task<T> GetAsync(ObjectId id);
+
+        /// <summary>
+        /// Gets document by Id.
+        /// </summary>
+        /// <param name="id">Key</param>
+        /// <returns>Document</returns>
+        T Get(ObjectId id);
 
         /// <summary>
         /// Gets document by condition specified or gets all documents if condition is not passed. Paged records.
@@ -43,6 +57,15 @@ namespace TK.MongoDB.Data
         Task<Tuple<IEnumerable<T>, long>> GetAsync(int currentPage, int pageSize, Expression<Func<T, bool>> condition = null);
 
         /// <summary>
+        /// Gets document by condition specified or gets all documents if condition is not passed. Paged records.
+        /// </summary>
+        /// <param name="currentPage">Page number</param>
+        /// <param name="pageSize">Page size</param>
+        /// <param name="condition">Lamda expression</param>
+        /// <returns>Tuple of records and total number of records</returns>
+        Tuple<IEnumerable<T>, long> Get(int currentPage, int pageSize, Expression<Func<T, bool>> condition = null);
+
+        /// <summary>
         /// Gets document by filter specified. Paged records.
         /// </summary>
         /// <param name="currentPage">Page number</param>
@@ -51,6 +74,16 @@ namespace TK.MongoDB.Data
         /// <param name="sort">Sort definition</param>
         /// <returns>Tuple of records and total number of records</returns>
         Task<Tuple<IEnumerable<T>, long>> GetAsync(int currentPage, int pageSize, FilterDefinition<T> filter = null, SortDefinition<T> sort = null);
+
+        /// <summary>
+        /// Gets document by filter specified. Paged records.
+        /// </summary>
+        /// <param name="currentPage">Page number</param>
+        /// <param name="pageSize">Page size</param>
+        /// <param name="filter">Filter definition</param>
+        /// <param name="sort">Sort definition</param>
+        /// <returns>Tuple of records and total number of records</returns>
+        Tuple<IEnumerable<T>, long> Get(int currentPage, int pageSize, FilterDefinition<T> filter = null, SortDefinition<T> sort = null);
 
         /// <summary>
         /// Gets document by condition specified or gets all documents if condition is not passed. Nonpaged records.
@@ -76,11 +109,25 @@ namespace TK.MongoDB.Data
         Task<T> InsertAsync(T instance);
 
         /// <summary>
+        /// Inserts single record.
+        /// </summary>
+        /// <param name="instance">Document</param>
+        /// <returns>Document</returns>
+        T Insert(T instance);
+
+        /// <summary>
         /// Updates single record based on Id.
         /// </summary>
         /// <param name="instance">Document</param>
         /// <returns></returns>
         Task<bool> UpdateAsync(T instance);
+
+        /// <summary>
+        /// Updates single record based on Id.
+        /// </summary>
+        /// <param name="instance">Document</param>
+        /// <returns></returns>
+        bool Update(T instance);
 
         /// <summary>
         /// Deletes record based on Id hard or soft based on logical value.
@@ -91,6 +138,14 @@ namespace TK.MongoDB.Data
         Task<bool> DeleteAsync(ObjectId id, bool logical = true);
 
         /// <summary>
+        /// Deletes record based on Id hard or soft based on logical value.
+        /// </summary>
+        /// <param name="id">Key</param>
+        /// <param name="logical">Soft delete</param>
+        /// <returns></returns>
+        bool Delete(ObjectId id, bool logical = true);
+
+        /// <summary>
         /// Counts documents based on condition specifed or counts all documents if condition is not passed.
         /// </summary>
         /// <param name="condition">Lamda expression</param>
@@ -98,10 +153,24 @@ namespace TK.MongoDB.Data
         Task<long> CountAsync(Expression<Func<T, bool>> condition = null);
 
         /// <summary>
+        /// Counts documents based on condition specifed or counts all documents if condition is not passed.
+        /// </summary>
+        /// <param name="condition">Lamda expression</param>
+        /// <returns></returns>
+        long Count(Expression<Func<T, bool>> condition = null);
+
+        /// <summary>
         /// Checks if the document exists based on the condition specified.
         /// </summary>
         /// <param name="condition">Lamda expression</param>
         /// <returns></returns>
         Task<bool> ExistsAsync(Expression<Func<T, bool>> condition);
+
+        /// <summary>
+        /// Checks if the document exists based on the condition specified.
+        /// </summary>
+        /// <param name="condition">Lamda expression</param>
+        /// <returns></returns>
+        bool Exists(Expression<Func<T, bool>> condition);
     }
 }
